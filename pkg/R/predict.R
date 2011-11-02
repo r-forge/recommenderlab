438c0a7e05dc9128409317f605a88d0d597e125c
hahsler (Michael Hahsler) <mhahsler@lyle.smu.edu>
2011-11-02 01:04:06 +0000

    move 2.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/predict.R
+++
@@ -1,6 +0,0 @@
-setMethod("predict", signature(object = "Recommender"),
-	function(object, newdata, n = 10, type="topNList", ...) 
-	object@predict(object@model, newdata, n = n, type= type, ...)
-)
-
-
