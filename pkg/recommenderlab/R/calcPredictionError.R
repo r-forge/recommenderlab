03943893c90f477a8c43fd4f7e317f2a11e889ee
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-09 21:10:15 +0000

    Release.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/recommenderlab/R/calcPredictionError.R
+++ pkg/recommenderlab/R/calcPredictionError.R
@@ -1,15 +1,15 @@
 setMethod("calcPredictionError", signature(x= "realRatingMatrix", 
 		data = "realRatingMatrix"),
 
-	function(x, data, byUser=TRUE) {
+	function(x, data, byUser=FALSE) {
 	    if(byUser) fun <- rowMeans
 	    else fun <- mean
 
-		MAD <- fun(abs(as(x, "matrix") - as(data,"matrix")), 
+	    MAE <- fun(abs(as(x, "matrix") - as(data,"matrix")), 
 		    na.rm=TRUE)
 	    MSE <- fun((as(x, "matrix") - as(data,"matrix"))^2, 
 		    na.rm=TRUE)
 	    RMSE <- sqrt(MSE)
 
-	    cbind(MAD, MSE, RMSE)
+	    cbind(MAE, MSE, RMSE)
 	})
