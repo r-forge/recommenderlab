86301e21ed70cf87a5dbd0baf14fb896198f7e18
aradhya (Akshaya Aradhya V) <aksy.aradhya@gmail.com>
2012-01-19 03:41:13 +0000

    Testing slopeOne in R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/recommenderlabMahout/R/RECOM_MAHOUT.R
+++ pkg/recommenderlabMahout/R/RECOM_MAHOUT.R
@@ -1,31 +1,56 @@
-## always recommends the top-N popular items (without known items)
-MAHOUT <- function(data, parameter = NULL) {
+library(recommenderlabMahout)
+data(MovieLense)
 
-    model <- list( data???)
+### use write.table
+write.csv(file = "data.csv",  as(MovieLense[1:20,], "data.frame"))
+
+
+data <- .jnew("java/lang/String", file)
+
+## always recommends the top-N popular items (without known items)
+MAHOUT_SLOPEONE <- function(data, parameter = NULL) {
+
+    model <- list( data )
 
     predict <- function(model, newdata, n=10, ...) {
 	
-	### code goes here.
-	write/read data
-	createRecommender (Java)
-	package item ids as a topNList	
+	##do a rbind newdata with data in model
+
+
+### use write.table
+### write.csv(file = "data.csv",  as(MovieLense[1:20,], "data.frame"))
+write.csv(file = "data.csv",  as(complete data with newdata), "data.frame"))
+
+data <- .jnew("java/lang/String", file)
+
+#recsystreeclustering <- .jnew("com/movieRecommender/test")
+#result =.jcall(recsystreeclustering,"[I","createRecommender","treeClusteringRecommender",file, as.integer(6012))
+#create a for loop with ids of those users. Return a prediction object. 
+#user <- 6012
+
+obj <- .jnew("com/movieRecommender/test")
+result = .jcall(obj,"[I","createRecommender","slopeOneRecommender",data, as.integer(user))
+result
 	
-	
+	#convert result (int array) to an object in topN 
+	#item labels are obtained from data
+	topN <- new(“topNList”, list(user1 = result), colnames(model$data),     		10L)
 	topN <- removeKnownItems(model$topN, newdata, replicate=TRUE)
-	topN <- bestN(topN, n)
+	topN <- bestN(topN, n) //n is taken from predict
 	return(topN)
     }
 
     ## construct recommender object
-    new("Recommender", method = "POPULAR", dataType = class(data),
+    new("Recommender", method = " MAHOUT_SLOPEONE", dataType = class(data),
 	    ntrain = nrow(data), model = model, predict = predict)
 }
 
 ## register recommender
 recommenderRegistry$set_entry(
-	method="MAHOUT", dataType = "realRatingMatrix", fun=MAHOUT, 
+	method="MAHOUT_SLOPEONE", dataType = "realRatingMatrix", fun= MAHOUT_SLOPEONE, 
 	description="Recommender based on Mahout."
 )
+
 
 
 
