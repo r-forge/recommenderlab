54bb40d9d67b717bcdc06b377b8d60a4b817b615
aradhya (Akshaya Aradhya V) <aksy.aradhya@gmail.com>
2012-04-02 02:30:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@58 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/recommenderlabMahout/R/RECOM_KNN.R
@@ -0,0 +1,35 @@
+ test_code <- function() {
+  library(recommenderlabMahout)
+  data("MovieLense")
+     rec <- Recommender(MovieLense, method = "MAHOUT_KNN")
+     pre <- predict(rec, 3, n = 10)
+     pre
+     as(pre, "list")
+}
+
+MAHOUT_KNN <- function(data, parameter = NULL) {
+
+    model <- list(data= data)
+    predict <- function(model, newdata, n=10, ...) {
+
+file <- "/tmp/data.csv"
+write.table(getData.frame(MovieLense[1:5], decode=FALSE), file=file, quote=FALSE, row.names=FALSE, col.names=FALSE, sep=",")
+data <- .jnew("java/lang/String", file)
+user <- newdata
+howmany <- 10
+obj <- .jnew("com/movieRecommender/Main")
+result <- .jcall(obj,"[I","createRecommender","knnRecommender",data, as.integer(user), as.integer(howmany))
+	topN <- new("topNList", items = list(user = result), itemLabels = colnames(model$data), n=length(result))
+	topN <- bestN(topN, n) 
+	return(topN)
+    }
+
+    new("Recommender", method = "MAHOUT_KNN", dataType = class(data),
+	    ntrain = nrow(data), model = model, predict = predict)
+}
+
+#recommenderRegistry$modify_entry(
+recommenderRegistry$set_entry(
+	method="MAHOUT_KNN", dataType = "realRatingMatrix", fun= MAHOUT_KNN, 
+	description="Recommender based on Mahout."
+)
