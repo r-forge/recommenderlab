bd9eb7b82c9b7445a1ca21ed18e7b3dad5daed39
dphanekham (Derek Phanekham) <dphanekham@smu.edu>
2013-09-23 16:36:22 +0000

    small changes to the underlying java interface with prea
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/recommenderlabPREA/R/RECOM_PREA.R
+++ pkg/recommenderlabPREA/R/RECOM_PREA.R
@@ -1,3 +1,25 @@
+#NOTE: MUST PRESS SOURCE OR SOURCE ON SAVE AND THEN BUILD AND RELOAD FOR CHANGES TO TAKE EFFECT!
+
+
+##TO DO
+# check for memory
+#do for just one user
+#create TopN
+#make parameters a string and a string[], find way to display them to users
+#implement stuff for more advanced recommenders
+#determine if you actually need the split stuff if you want to learn from the whole database
+#maybe use a 0 or 1 as the value
+
+
+#ok. this is exactly what i need to do for this project
+#need to add a getTopN function for a specific user (this gets the top N recommendations for that user)
+#need to be able to predict for a specified set of users 
+#need to add a parameter to allow this to work for multiple prea functions
+
+
+#commands i need
+#a<-REAL_PREA(DATA)
+#predict(a,...DATA[1,])
 
 REAL_PREA <- function(data, parameter= NULL) {
   
@@ -11,46 +33,44 @@ REAL_PREA <- function(data, parameter= NULL) {
     minRating = NA
   ), parameter)
   
+  #trip is a triplet (i, j, x) representation of a sparse matrix
+  trip <- (as(as(MovieLense,"dgCMatrix"), "dgTMatrix")) #replace movie lense with data
   
-  trip <- (as(as(data,"dgCMatrix"), "dgTMatrix"))
-  
-  #.jinit()
-  #options(java.parameters = "-Xmx2048m")
-  #.jaddClassPath("/home/derek/Work/source/prea.jar")
-  #.jaddClassPath("/inst/java/prea.jar")
-  
+  #interface is a Java object of the type CFInterface
   interface <- .jnew("CFInterface", check=TRUE, silent=FALSE)
+  
   
   rowlength <- trip@Dim[1]
   columnlength <- trip@Dim[2]
   column <- (1:columnlength)
   row <- (1:rowlength)
     
+  #calls interface.createRatingMatrix(rowlength, columnlength, i[], j[], x[]);
+  #basically transfers the matrix to a Java reporesentation of a sparse matrix
   r <-.jcall(interface, returnSig = "LRecContainer;",
              "createRatingMatrix", rowlength, columnlength,
              trip@i, trip@j, trip@x, silent=FALSE, check=TRUE)
   
+  #These are the parameters for the the PREA functions in java
+  #TODO: get these from command line in the future
   strings <- .jarray( c("ItemAvg", "simple", "0.2"))
   
   
-  #this creates a recommender object in java with above parameters
+  #this creates a recommender object using the above settings
+  #stores it in 'r'
   r <- .jcall(interface, returnSig = "LRecContainer;","createRecommender", r, strings)
   
-
-  model <- c(list(description = "IBCF: Reduced similarity matrix", preaObject = r), p)
+  #This describes the model for R.
+  model <- c(list(description = "PREA: ", preaObject = r), p)
   
-  
-  #make already known values NA
-  #create TopN
-  #do for just one user
-  
+  #This is the predict function that will be 
   predict <- function(model, newdata, n = 10, data=NULL, type=c("topNList", "ratings"), ...) {
-    
-    #this should go in predict
+    "predicting things"
     r <- model$preaObject
     p <- sapply(.jcall(interface, returnSig = "[[D", "runRecommender", r), .jevalArray, silent=FALSE)
     
-    p
+    #return matrix 1 rating for each item and 1 row for each user
+    
   }
   
   ## construct recommender object
