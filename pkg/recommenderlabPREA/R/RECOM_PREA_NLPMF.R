aeb6bfde019283015b996706be6495ad63fcf0a4
dphanekham <dphanekham@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-05 18:34:04 +0000

    updated structure of recommenders and man page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/recommenderlabPREA/R/RECOM_PREA_NLPMF.R
+++ pkg/recommenderlabPREA/R/RECOM_PREA_NLPMF.R
@@ -16,7 +16,8 @@
   kernel_inv_wid = 1,
   kernel_var_rbf = 1,
   kernel_var_bias = 0.11,
-  kernel_var_wht = 5
+  kernel_var_wht = 5,
+  lazy = FALSE
 )
 
 
@@ -24,35 +25,13 @@
 REAL_PREA_NLPMF<- function(data, parameter= NULL) {
   
   param <- .get_parameters(.REAL_PREA_NLPMF_PARAM, parameter)
-  
-  #trip is a triplet (i, j, x) representation of a sparse matrix
-  RatingsAsTripletMatrix <- (as(as(data,"dgCMatrix"), "dgTMatrix")) 
-  colAndRowNames <- data@data@Dimnames
-  #interface is a Java object of the type CFInterface
+  strings <- .jarray( c(param$method, "simple", "0.2", param$feature_count, param$learning_rate, param$momentum,
+                        param$max_iteration, param$kernel_inv_wid, param$kernel_var_rbf,
+                        param$kernel_var_bias, param$kernel_var_wht))
   interface <- .jnew("CFInterface", check=TRUE, silent=FALSE)
   
-  rowlength <- RatingsAsTripletMatrix@Dim[1]
-  columnlength <- RatingsAsTripletMatrix@Dim[2]
-  param$range[1] = rowlength
-  param$range[2] = columnlength
-  
-  recommenderJObject <- NULL
-  model <- c(list(description = "PREA: NLPMF", preaObject = NULL, data=data), param)
-  
-  if(param$lazy == FALSE) {
-    #calls interface.createRatingMatrix(rowlength, columnlength, i[], j[], x[]);
-    #basically transfers the matrix to a Java reporesentation of a sparse matrix
-    ratingMat <-.jcall(interface, returnSig = "LRecContainer;",
-                       "createRatingMatrix", rowlength, columnlength,
-                       RatingsAsTripletMatrix@i, RatingsAsTripletMatrix@j, RatingsAsTripletMatrix@x, silent=FALSE, check=TRUE)
-    
-    strings <- .jarray( c(param$method, "simple", "0.2", param$feature_count, param$learning_rate, param$momentum,
-                          param$max_iteration, param$kernel_inv_wid, param$kernel_var_rbf,
-                          param$kernel_var_bias, param$kernel_var_wht))
-    recommenderJObject <- .jcall(interface, returnSig = "LRecContainer;","createRecommender", ratingMat, strings)
-    model <- c(list(description = "PREA: NLPMF", preaObject = recommenderJObject, data=data), param)
-    
-  }
+  #sets up recommender. method found in AAA.R
+  model <- recommender_setup(data, param, interface, strings)
   
   
   #This describes the model for R.
@@ -60,80 +39,12 @@ REAL_PREA_NLPMF<- function(data, parameter= NULL) {
   #produce a top N list
   #and produce a matrix of ratings
   predict <- function(model, newdata, n = 10, data=NULL, type=c("topNList", "ratings"), ...) {
+    strings <- .jarray( c(model$method, "simple", "0.2", model$feature_count, model$learning_rate, model$momentum,
+                          model$max_iteration, model$kernel_inv_wid, model$kernel_var_rbf,
+                          model$kernel_var_bias, model$kernel_var_wht))
+    result <- predict_help(model, newdata, n, type, strings, model$interface)
     
-    type <- match.arg(type)
-    r <- model$preaObject
-    OriginalData <- model$data
-    
-    if(class(newdata) == "integer") {
-      print("integer")
-      if(model$lazy == TRUE) {
-        strings <- .jarray( c(model$method, "simple", "0.2", model$feature_count, model$learning_rate, model$momentum,
-                              model$max_iteration, model$kernel_inv_wid, model$kernel_var_rbf,
-                              model$kernel_var_bias, model$kernel_var_wht))
-        recommenderJObject <- .jcall(interface, returnSig = "LRecContainer;","createRecommender", ratingMat, strings)        
-        r <- recommenderJObject
-        
-      }
-      
-      predictedValues <- sapply(.jcall(interface, returnSig = "[[D", "runRecommender", r), .jevalArray, silent=FALSE)
-      predictedValues <- as(predictedValues, "realRatingMatrix")
-      predictedValues@data@Dimnames <- colAndRowNames
-      
-      if (type=="topNList") {
-        ratings <- predictedValues[newdata,]
-        return(getTopNLists(ratings, n))
-        
-      } else if (type == "ratings") {
-        print("compiling ratings")
-        return(predictedValues)
-      }
-      
-    }
-    
-    else if(class(newdata) == "realRatingMatrix") {
-      if(model$lazy == FALSE) stop("To add new data when predicting, rebuild the recommender with param=c(lazy=TRUE)")
-      
-      CombinedMatrix <- as(rbind2(as(newdata,"dgCMatrix"), as(OriginalData, "dgCMatrix")), "dgTMatrix")
-      CombinedColAndRowNames <- newdata@data@Dimnames
-      
-      CombinedRowlength <- CombinedMatrix@Dim[1]
-      CombinedColumnlength <- CombinedMatrix@Dim[2] 
-      
-      #calls interface.createRatingMatrix(rowlength, columnlength, i[], j[], x[]);
-      #basically transfers the matrix to a Java reporesentation of a sparse matrix
-      JavaRatingMat <-.jcall(interface, returnSig = "LRecContainer;",
-                             "createRatingMatrix", CombinedRowlength, CombinedColumnlength,
-                             CombinedMatrix@i, CombinedMatrix@j, CombinedMatrix@x, silent=FALSE, check=TRUE)
-      
-      strings <- .jarray( c(model$method, "simple", "0.2", model$feature_count, model$learning_rate, model$momentum,
-                            model$max_iteration, model$kernel_inv_wid, model$kernel_var_rbf,
-                            model$kernel_var_bias, model$kernel_var_wht))
-      recommenderJObject <- .jcall(interface, returnSig = "LRecContainer;","createRecommender", JavaRatingMat, strings)
-      #model <- c(list(description = "PREA: PMF", preaObject = recommenderJObject), model)
-      r <- recommenderJObject
-      
-      #calls interface.createRatingMatrix(rowlength, columnlength, i[], j[], x[]);
-      #basically transfers the matrix to a Java representation of a sparse matrix
-      predictedValues <- sapply(.jcall(interface, returnSig = "[[D", "runRecommender", r), .jevalArray, silent=FALSE)
-      predictedValues <- as(predictedValues, "realRatingMatrix")
-      predictedValues@data@Dimnames <- CombinedColAndRowNames
-      
-      if (type=="topNList") {
-        ratings <- predictedValues[1:newdata@data@Dim[1]+1,]
-        return(getTopNLists(ratings, n))
-        
-      } else if (type == "ratings") {
-        print("compiling ratings")
-        return(predictedValues[1:newdata@data@Dim[1]+1,])
-      }
-      
-      
-    }
   }
-
-  
-
   
   ## construct recommender object
   new("Recommender", method = "PREA_NLPMF", dataType = class(data),
