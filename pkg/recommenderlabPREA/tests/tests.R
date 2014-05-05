168b428efa2fed8c9c71a70aa4cf0b35021a2348
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-05 21:34:50 +0000

    Improved test.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recommenderlab@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/recommenderlabPREA/tests/tests.R
+++ pkg/recommenderlabPREA/tests/tests.R
@@ -1,73 +1,31 @@
 library(recommenderlabPREA)
-
-#PMF
+library(testthat)
 
 data("MovieLense")
 MovieLense100 <- MovieLense[rowCounts(MovieLense) >100, 1:50]
-rec <- Recommender(MovieLense100, method = "PREA_PMF")
-if(class(rec) != "Recommender") stop("create PMF recommender failed")
-
-## create top-N recommendations for users 1 to 10
-pre <- predict(rec, 1:10, type="topN")
-if(class(pre) != "topNList") stop("PMF predict topNList failed")
-
-## predict ratings for users 1 to 10
-pre <- predict(rec, 1:10, type="ratings")
-if(class(pre) != "realRatingMatrix") stop("PMF predict ratings failed")
-
-#SLOPEONE
-
-rec <- Recommender(MovieLense100, method = "PREA_SLOPEONE")
-if(class(rec) != "Recommender") stop("create SLOPEONE recommender failed")
-
-## create top-N recommendations for users 1 to 10
-pre <- predict(rec, 1:10, type="topN")
-if(class(pre) != "topNList") stop("SLOPEONE predict topNList failed")
-
-## predict ratings for users 1 to 10
-pre <- predict(rec, 1:10, type="ratings")
-if(class(pre) != "realRatingMatrix") stop("SLOPEONE predict ratings failed")
-
-#USERAVG
-
-rec <- Recommender(MovieLense100, method = "PREA_USERAVG")
-if(class(rec) != "Recommender") stop("create USERAVG recommender failed")
-
-## create top-N recommendations for users 1 to 10
-pre <- predict(rec, 1:10, type="topN")
-if(class(pre) != "topNList") stop("USERAVG predict topNList failed")
-
-## predict ratings for users 1 to 10
-pre <- predict(rec, 1:10, type="ratings")
-if(class(pre) != "realRatingMatrix") stop("USERAVG predict ratings failed")
-
-"REGSVD"
-
-rec <- Recommender(MovieLense100, method = "PREA_REGSVD")
-if(class(rec) != "Recommender") stop("create REGSVD recommender failed")
-
-## create top-N recommendations for users 1 to 10
-pre <- predict(rec, 1:10, type="topN")
-if(class(pre) != "topNList") stop("REGSVD predict topNList failed")
-
-## predict ratings for users 1 to 10
-pre <- predict(rec, 1:10, type="ratings")
-if(class(pre) != "realRatingMatrix") stop("REGSVD predict ratings failed")
-
-"NPCA"
-
-rec <- Recommender(MovieLense100, method = "PREA_NPCA")
-if(class(rec) != "Recommender") stop("create NPCA recommender failed")
-
-## create top-N recommendations for users 1 to 10
-pre <- predict(rec, 1:10, type="topN")
-if(class(pre) != "topNList") stop("NPCA predict topNList failed")
-
-## predict ratings for users 1 to 10
-pre <- predict(rec, 1:10, type="ratings")
-if(class(pre) != "realRatingMatrix") stop("NPCA predict ratings failed")
+train <- MovieLense100[1:50,] 
+test <- MovieLense100[51:53,] 
 
 
+methods <- grep("^PREA_", recommenderRegistry$get_entry_names(), value=TRUE)
+methods <- gsub("_realRatingMatrix", "", methods)
+methods
 
+for (m in methods) {
+  test_that(m, rec <- Recommender(train, method = m))
+  expect_is(rec, "Recommender")
+  
+  test_that(m, pre <- predict(rec, 1:3, type="topNList"))
+  expect_is(pre, "topNList")
+  
+  test_that(m, pre <- predict(rec, 1:3, type="ratings"))
+  expect_is(pre, "ratings")
+  
+  test_that(m, pre <- predict(rec, test[1:3,], type="topNList"))
+  expect_is(pre, "topNList")
+  
+  test_that(m, pre <- predict(rec, test[1:3,], type="ratings"))
+  expect_is(pre, "ratings")
+}
 
 
